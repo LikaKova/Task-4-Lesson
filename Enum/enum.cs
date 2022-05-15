7152266ef1c6104842b8b74174cd72bb6df6cc2a
akovalenko2 <jonnyconty@gmail.com>
2022-05-15 23:59:30 +0200

    Third commit

---
+++ Enum/enum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+	enum Errors
+	{
+		Informational,    // 0
+		Success,          // 1
+		Redirection,      // 2
+		ClientSide,       // 3
+		ServerSide        // 4
+
+	}
+
+	public static void Main()
+	{
+		int myError = (int)Errors.Success;
+		Console.WriteLine(myError);
+
+		Errors myError1 = Errors.Success;
+		Console.WriteLine(myError1);
+	}
+}
+
