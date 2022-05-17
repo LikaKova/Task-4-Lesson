226ac43ca127e7c082ac27a15d14c5593f543184
akovalenko2 <jonnyconty@gmail.com>
2022-05-17 21:51:44 +0200

    Added h1 tag

---
+++ TextReadTask/Read.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextReadTask
+
+	{
+		class ReadText
+		{
+			static void Main(string[] args)
+
+			{
+				string writeText = "ENV=QA";
+				File.WriteAllText("input.txt", writeText);
+
+				string readText = File.ReadAllText("input.txt");
+				Console.WriteLine(readText);
+
+			string writeTextNew = "I will run test on " + writeText;
+			File.WriteAllText("output.txt", writeTextNew);
+
+			string readTextNew = File.ReadAllText("output.txt");
+			Console.WriteLine(readTextNew);
+
+
+
+			}
+
+		}
+	}
+
